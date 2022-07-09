a607127a29308e41a684b522d4f1561835d60dc8
Rich Warner <richwarneriii@gmail.com>
2022-07-08 23:50:48 -0600

    testing

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require("express");
+import express from 'express';
+// import cors from "cors"; // for CORS setup, usage: app.use(cors());
 
 const app = express();
+const port = process.env.PORT || 3030; // default port to listen
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get('/', (req, res) => {
+  const randomId = `${Math.random()}`.slice(2);
+  const path = `item/${randomId}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Fetch one item: <a href="${path}">${path}</a>`);
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+app.get('/item/:itemId', (req, res) => {
+  const { itemId } = req.params;
+  res.json({ itemId });
 });
 
-// Export the Express API
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
+
 module.exports = app;
