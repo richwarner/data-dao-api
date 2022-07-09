dfe18ce0708e174ff5be32600c304a3433b3b405
Rich Warner <richwarneriii@gmail.com>
2022-07-09 14:44:27 -0600

    added database

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-import express from 'express';
-// import cors from "cors"; // for CORS setup, usage: app.use(cors());
+// Define "require"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const database = require('./database.json');
 
+/* Begin server section */
+import express from 'express';
+
+import cors from "cors"; // for CORS setup, usage: app.use(cors());
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3030; // default port to listen
+/* End server section */
+
+/* Begin storage api section */
+import { Web3Storage } from 'web3.storage'
+
+// Construct with token and endpoint
+const client = new Web3Storage({ token: process.env.STORAGE_API_TOKEN })
+
+// const fileInput = document.querySelector('input[type="file"]')
+
+// // Pack files into a CAR and send to web3.storage
+// const rootCid = await client.put(fileInput.files) // Promise<CIDString>
+
+// // Get info on the Filecoin deals that the CID is stored in
+// const info = await client.status(rootCid) // Promise<Status | undefined>
+
+// // Fetch and verify files from web3.storage
+// const res = await client.get(rootCid) // Promise<Web3Response | null>
+// const files = await res.files() // Promise<Web3File[]>
+// for (const file of files) {
+//   console.log(`${file.cid} ${file.name} ${file.size}`)
+// }
+/*  End storage api section */
 
 app.get('/', (req, res) => {
   const randomId = `${Math.random()}`.slice(2);
@@ -12,13 +42,23 @@ app.get('/', (req, res) => {
   res.end(`Hello! Fetch one item: <a href="${path}">${path}</a>`);
 });
 
+app.get('/items', (req, res) => {
+    res.json(database);
+  });
+
 app.get('/item/:itemId', (req, res) => {
   const { itemId } = req.params;
   res.json({ itemId });
+});
+
+app.post("/item", async function (req, res) {
+    const { itemId, itemName } = req.body;
+    //do something
+    res.send("Something done");
 });
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
 
-module.exports = app;
+export default app;
